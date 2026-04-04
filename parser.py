4ec1922768ff4038432dbd32359cc168cda77d61
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-03 18:51:38 -0600

    Add update subcommand to parser

--- parser.py
+++ parser.py
@@ -14,4 +14,6 @@ sub_parser = parser.add_subparsers(
 create_parser = sub_parser.add_parser("create", help="Create a new task")
 create_parser.add_argument("description", help="Description for the new task", type=str)
 
-
+update_parser = sub_parser.add_parser("update", help="Update a tasks decription")
+update_parser.add_argument("Task ID", help="ID of the task to update", type=int)
+update_parser.add_argument("Decription", help="New description for the task", type=str)
