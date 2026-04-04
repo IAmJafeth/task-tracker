11827412b6059d7d538ed7f12bc6d21ce234f253
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-04 11:09:46 -0600

    Normalize parser arguments and add types
    Use snake_case dest names (task_id, decription) for consistency.
    Add type=int to ID arguments to ensure integer parsing and add
    metavar values to preserve user-visible labels like "Task ID" and
    "Description".

--- parser.py
+++ parser.py
@@ -15,16 +15,16 @@ create_parser = sub_parser.add_parser("create", help="Create a new task")
 create_parser.add_argument("description", help="Description for the new task", type=str)
 
 update_parser = sub_parser.add_parser("update", help="Update a tasks decription")
-update_parser.add_argument("Task ID", help="ID of the task to update", type=int)
-update_parser.add_argument("Decription", help="New description for the task", type=str)
+update_parser.add_argument("task_id", help="ID of the task to update", type=int, metavar="Task ID")
+update_parser.add_argument("decription", help="New description for the task", type=str, metavar="Description")
 
 list_parser = sub_parser.add_parser("list", help="List all tasks")
 
 delete_parser = sub_parser.add_parser("delete", help="Delete a task")
-delete_parser.add_argument("Task ID", help="ID of the task to be deleted")
+delete_parser.add_argument("task_id", help="ID of the task to be deleted", type=int, metavar="Task ID")
 
 mark_in_progress_parser = sub_parser.add_parser("mark-in-progress", help="Mark a Task as in-progress")
-mark_in_progress_parser.add_argument("Task ID", help="ID of the task to be marked as in-progress")
+mark_in_progress_parser.add_argument("task_id", help="ID of the task to be marked as in-progress", type=int, metavar="Task ID")
 
 mark_done_parser = sub_parser.add_parser("mark-done", help= "Mark a task as done")
-mark_done_parser.add_argument("Task ID", help="ID of the task to be marked as done")
+mark_done_parser.add_argument("task_id", help="ID of the task to be marked as done", type=int, metavar="Task ID")
