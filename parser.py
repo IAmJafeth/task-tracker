477b12d6c6ae74345cf25fc01db83dc0da187825
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-04 21:32:04 -0600

    Refactor argparse and rename create to add

--- parser.py
+++ parser.py
@@ -1,10 +1,12 @@
 import argparse
 import tomllib
 
+
 def get_app_version() -> str:
     with open("pyproject.toml", "rb") as f:
         data = tomllib.load(f)
     return data.get("project", {}).get("version")
+
 
 parser = argparse.ArgumentParser(
     prog="Task-Tracker",
@@ -12,28 +14,45 @@ parser = argparse.ArgumentParser(
     epilog="A project from @Roadmap.sh",
 )
 
-parser.add_argument("-v", "--version", help="Return the task-tracker version installed", action="version", version=get_app_version())
-
-sub_parser = parser.add_subparsers(
-    required=True,
-    title="Action to take",
-    dest="action"
+parser.add_argument(
+    "-v",
+    "--version",
+    help="Return the task-tracker version installed",
+    action="version",
+    version=get_app_version(),
 )
 
-create_parser = sub_parser.add_parser("create", help="Create a new task")
-create_parser.add_argument("description", help="Description for the new task", type=str)
+sub_parser = parser.add_subparsers(required=True, title="Action to take", dest="action")
+
+add_parser = sub_parser.add_parser("add", help="Add a new task")
+add_parser.add_argument("description", help="Description for the new task", type=str)
 
 update_parser = sub_parser.add_parser("update", help="Update a tasks decription")
-update_parser.add_argument("task_id", help="ID of the task to update", type=int, metavar="Task ID")
-update_parser.add_argument("decription", help="New description for the task", type=str, metavar="Description")
+update_parser.add_argument(
+    "task_id", help="ID of the task to update", type=int, metavar="Task ID"
+)
+update_parser.add_argument(
+    "decription", help="New description for the task", type=str, metavar="Description"
+)
 
 list_parser = sub_parser.add_parser("list", help="List all tasks")
 
 delete_parser = sub_parser.add_parser("delete", help="Delete a task")
-delete_parser.add_argument("task_id", help="ID of the task to be deleted", type=int, metavar="Task ID")
+delete_parser.add_argument(
+    "task_id", help="ID of the task to be deleted", type=int, metavar="Task ID"
+)
 
-mark_in_progress_parser = sub_parser.add_parser("mark-in-progress", help="Mark a Task as in-progress")
-mark_in_progress_parser.add_argument("task_id", help="ID of the task to be marked as in-progress", type=int, metavar="Task ID")
+mark_in_progress_parser = sub_parser.add_parser(
+    "mark-in-progress", help="Mark a Task as in-progress"
+)
+mark_in_progress_parser.add_argument(
+    "task_id",
+    help="ID of the task to be marked as in-progress",
+    type=int,
+    metavar="Task ID",
+)
 
-mark_done_parser = sub_parser.add_parser("mark-done", help= "Mark a task as done")
-mark_done_parser.add_argument("task_id", help="ID of the task to be marked as done", type=int, metavar="Task ID")
+mark_done_parser = sub_parser.add_parser("mark-done", help="Mark a task as done")
+mark_done_parser.add_argument(
+    "task_id", help="ID of the task to be marked as done", type=int, metavar="Task ID"
+)
