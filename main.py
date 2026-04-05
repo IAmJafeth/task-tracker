9de935c5dbaa359019f477f3864ab84d82428ff0
Jafeth Garro <45522320+IAmJafeth@users.noreply.github.com>
2026-04-04 21:50:51 -0600

    Add basic CLI parsing and 'add' command

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+from parser import parser
+from task import Task, TaskList
+
 def main():
-    pass
+    task_list = TaskList()
+    task_list.create_task("Test Task")
+    args = parser.parse_args()
+    
+    action: str = args.action
+    
+    match action:
+        case "add":
+            task: Task = task_list.create_task(args.description)
+            print(f"Task added succesfully (ID: {task.id})")
+        
+        case _:
+            print("Error: Invalid command")
 
 if __name__ == "__main__":
     main()
